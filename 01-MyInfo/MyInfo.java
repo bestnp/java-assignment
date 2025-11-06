c527db81d9117b1aeecb4f2e76c713313d6645c4
bestnp <narawit.p40@gmail.com>
2025-11-06 10:29:24 +0700

    Initial commit with project setup and basic structure established.

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,12 @@
+public class MyInfo {   
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int age = 20;
+        String hobby = "Playing football";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+    }
+}
+
